31c622e1b1c39c96f6469ad1300503500b062ae2
bitchybratt <141075933+bitchybratt@users.noreply.github.com>
2023-10-01 22:17:28 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,23 +1,19 @@
-// Escuchar el evento submit del formulario
 document.getElementById("registroForm").addEventListener("submit", function (e) {
   e.preventDefault(); // Evitar el comportamiento predeterminado del formulario
 
-  // Obtener los valores del formulario
   const nombre = document.getElementById("nombre").value;
   const apellido = document.getElementById("apellido").value;
   const fechaNacimiento = document.getElementById("fechaNacimiento").value;
 
-  // Crear un objeto con los datos del formulario
+  // Creo objeto con los datos del formulario
   const formData = {
     Nombre: nombre,
     Apellido: apellido,
     FechaNacimiento: fechaNacimiento,
   };
 
-  // URL del endpoint donde enviarás la solicitud POST
   const url = "https://jsonplaceholder.typicode.com/users";
 
-  // Configuración de la solicitud
   const requestOptions = {
     method: "POST",
     headers: {
@@ -26,16 +22,15 @@ document.getElementById("registroForm").addEventListener("submit", function (e) 
     body: JSON.stringify(formData),
   };
 
-  // Realizar la solicitud POST
+  // Realizoo la solicitud POST
   fetch(url, requestOptions)
     .then((response) => response.json())
     .then((data) => {
       console.log("Respuesta del servidor:", data);
-      alert("Registro exitoso");
-      // Puedes redirigir a otra página o realizar otras acciones después del registro exitoso.
+      alert("Te registraste correctamente");
     })
     .catch((error) => {
       console.error("Error al enviar la solicitud:", error);
-      alert("Error al registrar");
+      alert("Error al registrarse");
     });
 });
